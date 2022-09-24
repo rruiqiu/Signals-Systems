d7950fb6f10332bcec48b0e0b0b61b3dd07babdc
Richard Qiu <qiur12@mcmaster.ca>
2022-09-23 20:12:15 -0400

    add notes

--- lab1/Q3.m
+++ lab1/Q3.m
@@ -5,9 +5,10 @@ red= 5.* img(:,:,1);
 green= 5. * img(:,:,2);
 %don't need to change
 blue=img(:,:,3);
- 
+
+%reconstruct the image
 reconstructed_image=cat(3,red,green,blue);
 imshow(reconstructed_image);
 
-%
+%writing files
 imwrite(reconstructed_image, 'my_fixed_image.jpg');
