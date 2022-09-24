3f54baa745dd2a3f019481dea24adf4fa3971ad4
Richard Qiu <qiur12@mcmaster.ca>
2022-09-23 20:02:38 -0400

    update Q3

---
+++ lab1/Q3.m
@@ -0,0 +1,13 @@
+clear
+img = imread('ee3tp3picture2022.jpg');
+
+red= 5.* img(:,:,1);
+green= 5. * img(:,:,2);
+%don't need to change
+blue=img(:,:,3);
+ 
+reconstructed_image=cat(3,red,green,blue);
+imshow(reconstructed_image);
+
+%
+% imwrite(reconstructed_image, 'my_fixed_image.jpg');
