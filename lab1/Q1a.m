700b396e02b666e0821ac0ea78d5a1a45dbc8aaf
Richard Qiu <qiur12@mcmaster.ca>
2022-10-01 16:40:23 -0400

    fix

--- lab1/Q1a.m
+++ lab1/Q1a.m
@@ -10,6 +10,6 @@ axis([min(t)-1 max(t)+1 min(d)-0.1 max(d)+0.1]);
 ylabel('y');
 xlabel('x');
 title('question 1(a)');
-text(-9.5,-0.6,'Rui Qiu, ID:400318681, Shabab Rahman, ID:400291441','Fontsize',9);
+text(-10.5,-0.6,'Rui Qiu, ID:400318681, Shabab Rahman, ID:400291441','Fontsize',9);
 
 exportgraphics(gcf, 'Q1a.jpg');
